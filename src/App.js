e6c0cec09f271f38949a6205d04b83079ece5f07
sarthakx30 <sarthakkharabanda@gmail.com>
2022-06-25 14:05:54 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import Form from './components/form'
+import ToddoList from './components/TodoList';
 
 function App() {
+  const [inputText,setInputText]=useState("");
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Task Manager</h1>
+        <h4>{inputText}</h4>
       </header>
+      <Form setInputText={setInputText}/>
+      <ToddoList />
     </div>
   );
 }
---
+++ src/components/TodoList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const ToddoList=()=>{
+    return(
+        <div className="todo-container">
+            <ul className="todo-list">
+
+            </ul>
+        </div>
+    )
+}
+
+export default ToddoList;
